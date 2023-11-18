2de24e59657bc5f6565c962e278abb2cbe71b1a4
Patrick Gono <patrick.gono@gmail.com>
2023-11-18 19:24:19 +0100

    Refactor controller class, handle mouse and keyboard in seperate methods

--- src/controller.cpp
+++ src/controller.cpp
@@ -13,38 +13,53 @@ auto controller::handle_input(bool& running, camera& cam) -> void
         {
             running = false;
         } 
-        else if (event.type == SDL_KEYDOWN) 
+        handle_keyboard(event, cam);
+        handle_mouse(event, cam);
+    }
+}
+
+
+///
+///
+auto controller::handle_keyboard(const SDL_Event& event, camera& cam) -> void
+{
+    if (event.type == SDL_KEYDOWN) 
+    {
+        switch (event.key.keysym.sym) 
         {
-            switch (event.key.keysym.sym) 
-            {
-            case SDLK_UP:
-            case SDLK_w:
-                cam.translate_y(true);
-                break;
-            case SDLK_DOWN:
-            case SDLK_s:
-                cam.translate_y(false);
-                break;
-            case SDLK_LEFT:
-            case SDLK_a:
-                cam.translate_x(true);
-                break;
-            case SDLK_RIGHT:
-            case SDLK_d:
-                cam.translate_x(false);
-                break;
-            }
+        case SDLK_UP:
+        case SDLK_w:
+            cam.translate_y(true);
+            break;
+        case SDLK_DOWN:
+        case SDLK_s:
+            cam.translate_y(false);
+            break;
+        case SDLK_LEFT:
+        case SDLK_a:
+            cam.translate_x(true);
+            break;
+        case SDLK_RIGHT:
+        case SDLK_d:
+            cam.translate_x(false);
+            break;
         }
-        else if (event.type == SDL_MOUSEWHEEL)
+    }
+}
+
+///
+///
+auto controller::handle_mouse(const SDL_Event& event, camera& cam) -> void
+{
+    if (event.type == SDL_MOUSEWHEEL)
+    {
+        if (event.wheel.y > 0)
         {
-            if (event.wheel.y > 0)
-            {
-                cam.zoom(true);
-            }
-            else if (event.wheel.y < 0)
-            {
-                cam.zoom(false);
-            }
+            cam.zoom(true);
+        }
+        else if (event.wheel.y < 0)
+        {
+            cam.zoom(false);
         }
     }
 }
--- src/controller.h
+++ src/controller.h
@@ -1,8 +1,14 @@
 #pragma once
 #include "camera.h"
 
+#include "SDL.h"
+
 class controller 
 {
 public: // Interface
     static auto handle_input(bool& running, camera& cam) -> void;
+
+private: // Implementation
+    static auto handle_keyboard(const SDL_Event& event, camera& cam) -> void;
+    static auto handle_mouse(const SDL_Event& event, camera& cam) -> void;
 };
